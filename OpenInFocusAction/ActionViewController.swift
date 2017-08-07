35fa987923c90d4f64b32ff84264bc97c87438f7
Jeff Boek <jeff@jeffboek.com>
2017-08-07 16:13:29 -0700

    action extension now opens the focus deep link

--- OpenInFocusAction/ActionViewController.swift
+++ OpenInFocusAction/ActionViewController.swift
@@ -1,87 +1,91 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
+ 
  * License, v. 2.0. If a copy of the MPL was not distributed with this
+ 
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+
 import UIKit
+import Social
 import MobileCoreServices
 
-class ActionViewController: UIViewController {
+extension NSObject {
+    func callSelector(selector: Selector, object: AnyObject?, delay: TimeInterval) {
+        let delay = delay * Double(NSEC_PER_SEC)
+        let time = DispatchTime.init(uptimeNanoseconds: UInt64(delay))
+        DispatchQueue.main.asyncAfter(deadline: time) {
+            Thread.detachNewThreadSelector(selector, toTarget:self, with: object)
+        }
+    }
+}
 
-    @IBOutlet weak var imageView: UIImageView!
+class ActionViewController: SLComposeServiceViewController {
+    override func isContentValid() -> Bool {
+        return true
+    }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func didSelectPost() {
+        return
+    }
     
-        // Get the item[s] we're handling from the extension context.
+    override func configurationItems() -> [Any]! {
+        let item: NSExtensionItem = extensionContext!.inputItems[0] as! NSExtensionItem
+        let itemProvider: NSItemProvider = item.attachments![0] as! NSItemProvider
+        let type = kUTTypeURL as String
         
-        // For example, look for an image and place it into an image view.
-        // Replace this with something appropriate for the type[s] your extension supports.
-        guard let braveUrl = NSURL(string: "https://www.google.com") else { return }
-        let context = NSExtensionContext()
-        context.open(braveUrl as URL, completionHandler: nil)
-        print("HELLOOO1")
+        if itemProvider.hasItemConformingToTypeIdentifier(type) {
+            itemProvider.loadItem(forTypeIdentifier: type, options: nil, completionHandler: {
+                (urlItem, error) in
+                
+                guard let url = (urlItem as! NSURL).absoluteString?.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.alphanumerics),
+                    let braveUrl = NSURL(string: "firefox-focus://open-url?url=\(url)") else { return }
 
-        // From http://stackoverflow.com/questions/24297273/openurl-not-work-in-action-extension
-        var responder = self as UIResponder?
-        while (responder != nil) {
-            print("HELLOOO")
-            let selectorOpenURL = sel_registerName("openURL:")
-            if responder!.responds(to: selectorOpenURL) {
-                responder!.perform(selectorOpenURL, with: braveUrl)
-            }
-            responder = responder!.next
-        }
-        var imageFound = false
-        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
-            for provider in item.attachments! as! [NSItemProvider] {
-                if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
-                    // This is an image. We'll load it, then place it in our image view.
-                    weak var weakImageView = self.imageView
-                    provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { (imageURL, error) in
-                        OperationQueue.main.addOperation {
-                            if let strongImageView = weakImageView {
-                                if let imageURL = imageURL as? URL {
-                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
-                                }
-                            }
-                        }
-                    })
-                    
-                    imageFound = true
-                    break
+                // From http://stackoverflow.com/questions/24297273/openurl-not-work-in-action-extension
+                
+                var responder = self as UIResponder?
+                let selectorOpenURL = sel_registerName("openURL:")
+                while (responder != nil) {
+                    if responder!.responds(to: selectorOpenURL) {
+                        responder!.callSelector(selector: selectorOpenURL!, object: braveUrl, delay: 0)
+                    }
+                    responder = responder!.next
                 }
-            }
+                
+                
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.init(uptimeNanoseconds: UInt64(0.1 * Double(NSEC_PER_SEC)))) {
+                    
+                    self.cancel()
+                    
+                }
+                
+            })
             
-            if (imageFound) {
-                // We only handle one image, so stop looking for more.
-                break
-            }
         }
+        
+        
+        return []
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // Stop keyboard from showing
+        
+        textView.resignFirstResponder()
+        
+        textView.isEditable = false
+        
+        
+        super.viewDidAppear(animated)
+        
     }
-
-    @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-//        guard let url = (urlItem as! NSURL).absoluteString?.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.alphanumerics),
-           guard let braveUrl = NSURL(string: "firefox-focus://open-url?url=") else { return }
-
-        print("HELLOOO1")
-
-        // From http://stackoverflow.com/questions/24297273/openurl-not-work-in-action-extension
-        var responder = self as UIResponder?
-        while (responder != nil) {
-            print("HELLOOO")
-            if responder!.responds(to: #selector(UIApplication.canOpenURL(_:))) {
-                responder!.perform(#selector(UIApplication.canOpenURL(_:)), with: braveUrl)
-            }
-            responder = responder!.next
-        }
-        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    
+    
+    override func willMove(toParentViewController parent: UIViewController?) {
+        
+        view.alpha = 0
+        
     }
-
+    
 }
