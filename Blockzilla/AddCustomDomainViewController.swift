e92e1b5a3ba4e6dadae85dc02de9e1ed7e0c9975
Joe Gasiorek <joe.gasiorek@gmail.com>
2017-09-18 13:06:29 -0700

    Custom Domains: Color updates

--- Blockzilla/AddCustomDomainViewController.swift
+++ Blockzilla/AddCustomDomainViewController.swift
@@ -9,9 +9,9 @@ protocol AddCustomDomainDelegate {
     func addCustomDomainViewController(_ addCustomDomainViewController: AddCustomDomainViewController, domain: String)
 }
 
-class AddCustomDomainViewController: UIViewController, UITextViewDelegate {
+class AddCustomDomainViewController: UIViewController, UITextFieldDelegate {
     private var delegate: AddCustomDomainDelegate
-    private var textView = UITextView()
+    private var textInput = UITextField()
     
     init(delegate: AddCustomDomainDelegate) {
         self.delegate = delegate
@@ -27,32 +27,69 @@ class AddCustomDomainViewController: UIViewController, UITextViewDelegate {
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: UIConstants.strings.cancel, style: .plain, target: self, action: #selector(AddCustomDomainViewController.cancelTapped))
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(AddCustomDomainViewController.doneTapped))
         
+        setupUI()
+    }
+    
+    private func setupUI() {
         view.backgroundColor = UIConstants.colors.background
-        view.addSubview(textView)
+
         
-        textView.keyboardType = .URL
-        textView.autocapitalizationType = .none
-        textView.autocorrectionType = .no
-        textView.returnKeyType = .done
-        textView.textColor = UIColor.white
-        textView.delegate = self
-        textView.becomeFirstResponder()
-        textView.backgroundColor = UIConstants.colors.background
+        let container = UIView()
+        container.backgroundColor = UIConstants.colors.urlTextBackground
+        view.addSubview(container)
         
-        textView.snp.makeConstraints { (make) in
-            make.top.equalTo(50)
-            make.height.equalTo(40)
+        let httpLabel = UILabel()
+        httpLabel.text = "http://"
+        httpLabel.textColor = UIConstants.colors.settingsTextLabel
+        
+        container.addSubview(httpLabel)
+        container.addSubview(textInput)
+        
+        textInput.keyboardType = .URL
+        textInput.autocapitalizationType = .none
+        textInput.autocorrectionType = .no
+        textInput.returnKeyType = .done
+        textInput.textColor = UIColor.white
+        textInput.delegate = self
+        textInput.placeholder = "mozilla.org"
+        textInput.becomeFirstResponder()
+        
+        container.snp.makeConstraints { (make) in
             make.width.equalToSuperview()
+            make.height.equalTo(40)
+        }
+        
+        httpLabel.snp.makeConstraints { (make) in
+            make.height.equalToSuperview()
+            make.left.equalTo(10)
+            make.width.equalTo(50)
+            make.right.equalTo(httpLabel.snp.left)
+        }
+        
+        textInput.snp.makeConstraints { (make) in
+            make.height.equalToSuperview()
+            make.width.equalToSuperview()
+            make.left.equalTo(httpLabel.snp.right)
         }
     }
     
     @objc func cancelTapped() {
         self.navigationController?.popViewController(animated: true)
     }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        doneTapped()
+        return true
+    }
     
     @objc func doneTapped() {
         self.resignFirstResponder()
-        delegate.addCustomDomainViewController(self, domain: textView.text)
+        guard let domain = textInput.text else { return }
+        if domain.count == 0 {
+            return
+        }
+        
+        delegate.addCustomDomainViewController(self, domain: domain)
         self.navigationController?.popViewController(animated: true)
     }
 }
