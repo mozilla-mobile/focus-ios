bac7495aa1ed33fe87309d7fd6bfd72b6092ad7e
Krishna Kotecha <krishna.kotecha@logiccolony.com>
2017-10-04 13:32:03 +0100

    Add a comment to the UIPasteBoard extension method: -urlAsync(callback:). Explain the rationale for its existence (a workaround for a limitation/bug of the UIPasteboard API in relation to the Universal Clipboard).

--- Blockzilla/OverlayView.swift
+++ Blockzilla/OverlayView.swift
@@ -206,6 +206,22 @@ extension OverlayView: KeyboardHelperDelegate {
 }
 
 extension UIPasteboard {
+    
+    //
+    // As of iOS 11: macOS/iOS's Universal Clipboard feature causes UIPasteboard to block.
+    //
+    // (Known) Apple Radars that have been filed:
+    //
+    //  http://www.openradar.me/28787338
+    //  http://www.openradar.me/28774309
+    //
+    // Discussion on Twitter:
+    //
+    //  https://twitter.com/steipete/status/787985965432369152
+    //
+    //  To workaround this, urlAsync(callback:) makes a call of UIPasteboard.general on
+    //  an async dispatch queue, calling the completion block when done.
+    //
     func urlAsync(callback: @escaping (URL?) -> Void) {
         DispatchQueue.global().async {
             let url = URL(string: UIPasteboard.general.string ?? "")
