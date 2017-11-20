caede56a4562f174195c9aebf01edf8db63a6cc8
Aaron Train <aaron.train@gmail.com>
2017-11-20 18:43:34 -0500

    Initial review changes
    Added waits for certain elements before actions

--- XCUITest/TPSidebarBadge.swift
+++ XCUITest/TPSidebarBadge.swift
@@ -61,6 +61,9 @@ class TrackingProtectionSidebar: BaseTestCase {
         // Open the tracking protection sidebar
         app.otherElements["URLBar.trackingProtectionIcon"].tap()
 
+        // Wait for the sidebar to open
+        waitforExistence(element: app.staticTexts["Tracking Protection"])
+
         // Check for the tracker count in the sidebar
         counters = app.staticTexts.matching(identifier: "TrackingProtectionBreakdownItem.counterLabel")
         for i in 0..<counters.staticTexts.count {
@@ -71,6 +74,7 @@ class TrackingProtectionSidebar: BaseTestCase {
         app.buttons["TrackingProtectionView.closeButton"].tap()
 
         // Erase the history
+        waitforExistence(element: app.buttons["ERASE"])
         app.buttons["ERASE"].tap()
         waitforExistence(element: app.staticTexts["Your browsing history has been erased."])
     }
@@ -90,6 +94,7 @@ class TrackingProtectionSidebar: BaseTestCase {
         waitforExistence(element: app.staticTexts["Tracking Protection"])
 
         // Disable tracking protection
+        waitforExistence(element: app.switches["TrackingProtectionToggleView.toggleTrackingProtection"])
         app.switches["TrackingProtectionToggleView.toggleTrackingProtection"].tap()
 
         // Reopen the tracking protection sidebar
@@ -107,11 +112,13 @@ class TrackingProtectionSidebar: BaseTestCase {
 
         // Close the sidebar
         app.buttons["TrackingProtectionView.closeButton"].tap()
+        waitforNoExistence(element: app.staticTexts["TrackingProtectionToggleView.toggleTrackingProtection"])
 
         // Check that no counter exists (tracking protection: disabled)
         XCTAssertEqual(app.staticTexts["TrackingProtectionBadge.counterLabel"].exists, false)
 
         // Erase the history
+        waitforExistence(element: app.buttons["ERASE"])
         app.buttons["ERASE"].tap()
         waitforExistence(element: app.staticTexts["Your browsing history has been erased."])
     }
