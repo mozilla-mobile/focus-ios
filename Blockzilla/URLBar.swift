46ef14b787fd4c431c5f2cc28f238a0e7e7dd0d7
Aaron Train <aaron.train@gmail.com>
2017-11-27 14:54:25 -0500

    No semicolon required

--- Blockzilla/URLBar.swift
+++ Blockzilla/URLBar.swift
@@ -136,7 +136,7 @@ class URLBar: UIView {
         urlText.textColor = UIConstants.colors.urlTextFont
         urlText.highlightColor = UIConstants.colors.urlTextHighlight
         urlText.keyboardType = .webSearch
-        urlText.keyboardAppearance = UIKeyboardAppearance.dark;
+        urlText.keyboardAppearance = UIKeyboardAppearance.dark
         urlText.autocapitalizationType = .none
         urlText.autocorrectionType = .no
         urlText.rightView = clearButton
