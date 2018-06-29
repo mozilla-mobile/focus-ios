bb52cfb78192e7b847b1e69b2b92437e80c2ba75
oliviabrown9 <obrown917@gmail.com>
2018-06-29 09:57:33 -0700

    Reset app settings in set up

--- Blockzilla/AppDelegate.swift
+++ Blockzilla/AppDelegate.swift
@@ -19,6 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     static let prefWhatsNewCounter = "WhatsNewCounter"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        if AppInfo.testRequestsReset() {
+            if let bundleID = Bundle.main.bundleIdentifier {
+                UserDefaults.standard.removePersistentDomain(forName: bundleID)
+            }
+        }
         setupContinuousDeploymentTooling()
         setupErrorTracking()
         setupTelemetry()
