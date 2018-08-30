a09fbf966e9cb22c8122dbb8fe2af35cdfe16d68
oliviabrown9 <obrown917@gmail.com>
2018-08-30 16:32:49 -0700

    Added telemetry

--- Blockzilla/BrowserViewController.swift
+++ Blockzilla/BrowserViewController.swift
@@ -1092,10 +1092,15 @@ extension BrowserViewController: HomeViewDelegate {
     func tipTapped() {
         guard let tip = tipManager?.currentTip, tip.showVc else { return }
         switch tip.identifier {
-        case TipManager.TipKey.biometricTip, TipManager.TipKey.siriEraseTip:
+        case TipManager.TipKey.biometricTip:
+            Telemetry.default.recordEvent(category: TelemetryEventCategory.action, method: TelemetryEventMethod.click, object: TelemetryEventObject.biometricTip)
+            showSettings()
+        case TipManager.TipKey.siriEraseTip:
+            Telemetry.default.recordEvent(category: TelemetryEventCategory.action, method: TelemetryEventMethod.click, object: TelemetryEventObject.siriEraseTip)
             showSettings()
         case TipManager.TipKey.siriFavoriteTip:
             if #available(iOS 12.0, *) {
+                Telemetry.default.recordEvent(category: TelemetryEventCategory.action, method: TelemetryEventMethod.show, object: TelemetryEventObject.siriFavoriteTip)
                 showSiriFavoriteSettings()
             }
         default:
