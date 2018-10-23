fc4f9a52a0d32f9f5901049544081a8b7e398fa2
Botari <narshodan@hotmail.com>
2018-10-23 23:25:21 +0900

    Support both python 2.7 and >2.7

--- checkout.sh
+++ checkout.sh
@@ -4,6 +4,20 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-./build-disconnect.py
+if ! hash python; then
+    echo "python is not installed"
+    exit 1
+fi
+
+ver=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
+echo $ver
+if [ "$ver" -lt "27" ]; then
+    echo "This script requires python 2.7 or greater"
+    exit 1
+elif [ "$ver" -eq "27" ]; then
+    ./build-disconnect2.py
+elif [ "$ver" -gt "27" ]; then
+    ./build-disconnect3.py
+fi
 
 carthage bootstrap --platform iOS
