198ea1a03f4621dd04878ebf6b07b1198aa1d7da
Meng-Wei <mmeng.wei@mail.utoronto.ca>
2018-11-03 22:25:49 -0400

    Add suggestions and query tests

--- ClientTests/SearchEngineTests.swift
+++ ClientTests/SearchEngineTests.swift
@@ -26,52 +26,101 @@ class SearchEngineTests: XCTestCase {
         super.tearDown()
     }
 
+    func testEmptyStringQuery() {
+        let manager = SearchEngineManager(prefs: UserDefaults.standard)
+        let engine = manager.activeEngine
+        
+        let queryURL = engine.urlForQuery(EMPTY_STRING_SEARCH)
+        XCTAssertNil(queryURL)
+    }
+    
     func testEmptyStringSearchSuggestions() {
         let manager = SearchEngineManager(prefs: UserDefaults.standard)
         let engine = manager.activeEngine
         
-        let searchURL = engine.urlForQuery(EMPTY_STRING_SEARCH)
+        let searchURL = engine.urlForSuggestions(EMPTY_STRING_SEARCH)
         XCTAssertNil(searchURL)
+    }
+    
+    func testWhiteSpaceQuery() {
+        let manager = SearchEngineManager(prefs: UserDefaults.standard)
+        let engine = manager.activeEngine
+        
+        let queryURL = engine.urlForQuery(WHITE_SPACE_CHAR_SEARCH)
+        XCTAssertNil(queryURL)
     }
     
     func testWhiteSpaceSearchSuggestions() {
         let manager = SearchEngineManager(prefs: UserDefaults.standard)
         let engine = manager.activeEngine
         
-        let searchURL = engine.urlForQuery(WHITE_SPACE_CHAR_SEARCH)
+        let searchURL = engine.urlForSuggestions(WHITE_SPACE_CHAR_SEARCH)
         XCTAssertNil(searchURL)
+    }
+    
+    func testNewLineQuery() {
+        let manager = SearchEngineManager(prefs: UserDefaults.standard)
+        let engine = manager.activeEngine
+        
+        let queryURL = engine.urlForQuery(NEW_LINE_CHAR_SEARCH)
+        XCTAssertNil(queryURL)
     }
     
     func testNewLineSearchSuggestions() {
         let manager = SearchEngineManager(prefs: UserDefaults.standard)
         let engine = manager.activeEngine
         
-        let searchURL = engine.urlForQuery(NEW_LINE_CHAR_SEARCH)
+        let searchURL = engine.urlForSuggestions(NEW_LINE_CHAR_SEARCH)
         XCTAssertNil(searchURL)
+    }
+    
+    func testSpecialCharacterQuery() {
+        let manager = SearchEngineManager(prefs: UserDefaults.standard)
+        let engine = manager.activeEngine
+        
+        let queryURL = engine.urlForQuery(SPECIAL_CHAR_SEARCH)
+        XCTAssertNotNil(queryURL)
     }
     
     func testSpecialCharacterSearchSuggestions() {
         let manager = SearchEngineManager(prefs: UserDefaults.standard)
         let engine = manager.activeEngine
         
-        let searchURL = engine.urlForQuery(SPECIAL_CHAR_SEARCH)
+        let searchURL = engine.urlForSuggestions(SPECIAL_CHAR_SEARCH)
         XCTAssertNotNil(searchURL)
+    }
+    
+    func testNormalQuery() {
+        let manager = SearchEngineManager(prefs: UserDefaults.standard)
+        let engine = manager.activeEngine
+        
+        let queryURL = engine.urlForQuery(NORMAL_SEARCH)
+        XCTAssertNotNil(queryURL)
     }
     
     func testNormalSearchSuggestions() {
         let manager = SearchEngineManager(prefs: UserDefaults.standard)
         let engine = manager.activeEngine
         
-        let searchURL = engine.urlForQuery(NORMAL_SEARCH)
+        let searchURL = engine.urlForSuggestions(NORMAL_SEARCH)
         XCTAssertNotNil(searchURL)
+    }
+    
+    func testBeginWithWhiteSpaceQuery() {
+        let manager = SearchEngineManager(prefs: UserDefaults.standard)
+        let engine = manager.activeEngine
+        
+        let normalQueryURL = engine.urlForQuery(NORMAL_SEARCH)
+        let testQueryURL = engine.urlForQuery(BEGIN_WITH_WHITE_SPACE_SEARCH)
+        XCTAssertEqual(normalQueryURL, testQueryURL)
     }
     
     func testBeginWithWhiteSpaceSearchSuggestions() {
         let manager = SearchEngineManager(prefs: UserDefaults.standard)
         let engine = manager.activeEngine
         
-        let normalSearchURL = engine.urlForQuery(NORMAL_SEARCH)
-        let testSearchURL = engine.urlForQuery(BEGIN_WITH_WHITE_SPACE_SEARCH)
+        let normalSearchURL = engine.urlForSuggestions(NORMAL_SEARCH)
+        let testSearchURL = engine.urlForSuggestions(BEGIN_WITH_WHITE_SPACE_SEARCH)
         XCTAssertEqual(normalSearchURL, testSearchURL)
     }
 }
