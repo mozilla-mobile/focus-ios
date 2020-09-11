67fa4bd2836708c21988aa154044870b633bb95d
Nishant <nikieme3@gmail.com>
2020-09-11 15:48:17 -0400

    Temp

--- buddybuild_carthage_command.sh
+++ buddybuild_carthage_command.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 # https://github.com/Carthage/Carthage/issues/3003
-# brew uninstall --force carthage
+ brew uninstall --force carthage
 # brew install https://github.com/Homebrew/homebrew-core/raw/09ceff6c1de7ebbfedb42c0941a48bfdca932c0f/Formula/carthage.rb
-brew unlink carthage
-brew install https://github.com/Homebrew/homebrew-core/raw/a1222d4ca1e3b81374df2d7620f8d47503f6a8f2/Formula/carthage.rb
+#brew unlink carthage
+#brew install https://github.com/Homebrew/homebrew-core/raw/a1222d4ca1e3b81374df2d7620f8d47503f6a8f2/Formula/carthage.rb
+
+c7550f832f23d2c00bf0c014351719839593c641
+
+git clone https://github.com/Carthage/Carthage.git
+
+cd Carthage
+
+git checkout c7550f832f23d2c00bf0c014351719839593c641
+
+make install
+
+cd ..
 
 carthage version
 
