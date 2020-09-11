0d468f50df8a2f4b47a6b6b5bd2d36c2a7a68977
Nishant <nikieme3@gmail.com>
2020-09-11 16:59:44 -0400

    Added script to install carthage 0.34.0 due to BuddyBuild CI issue

--- buddybuild_carthage_command.sh
+++ buddybuild_carthage_command.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
 # https://github.com/Carthage/Carthage/issues/3003
-# brew uninstall --force carthage
-# brew install https://github.com/Homebrew/homebrew-core/raw/09ceff6c1de7ebbfedb42c0941a48bfdca932c0f/Formula/carthage.rb
-# brew unlink carthage
-# brew install https://github.com/Homebrew/homebrew-core/raw/a1222d4ca1e3b81374df2d7620f8d47503f6a8f2/Formula/carthage.rb
+#brew uninstall --force carthage
+#git clone https://github.com/Carthage/Carthage.git
+#cd Carthage
+#git checkout 25ad8c8ed71afe218aed9a7f7631543ce8adf858
+#make install
+#cd ..
 
+# https://github.com/Carthage/Carthage/issues/3003
 brew uninstall --force carthage
-
-git clone https://github.com/Carthage/Carthage.git
-
-cd Carthage
-
-git checkout 25ad8c8ed71afe218aed9a7f7631543ce8adf858
-
-make install
-
-cd ..
-
-#brew extract --version=0.34.0 carthage carthage/custom
-#brew install carthage@0.34.0
+wget https://github.com/Carthage/Carthage/releases/download/0.34.0/Carthage.pkg
+installer -pkg Carthage.pkg -target CurrentUserHomeDirectory
+cd /usr/local/bin && ln -s ~/usr/local/bin/carthage .
 
 carthage version
 
