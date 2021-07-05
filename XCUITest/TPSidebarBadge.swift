93122a96475539a7c97b08d5de240ce459a57d70
Bogdan Cernea <bogdan.cernea@softvision.com>
2021-07-05 15:57:37 +0300

    Fix tests

--- XCUITest/TPSidebarBadge.swift
+++ XCUITest/TPSidebarBadge.swift
@@ -52,8 +52,8 @@ class TrackingProtectionMenu: BaseTestCase {
         app.buttons["PhotonMenu.close"].tap()
 
         // Erase the history
-        waitForHittable(app.buttons["URLBar.deleteButton"])
-        app.buttons["URLBar.deleteButton"].tap()
+        waitForExistence(app.buttons["URLBar.deleteButton"])
+        app.buttons["URLBar.deleteButton"].firstMatch.tap()
         waitForExistence(app.staticTexts["Your browsing history has been erased."])
 
         // Load another website known for zero (0) trackers
@@ -74,8 +74,8 @@ class TrackingProtectionMenu: BaseTestCase {
         app.buttons["PhotonMenu.close"].tap()
 
         // Erase the history
-        waitForHittable(app.buttons["URLBar.deleteButton"])
-        app.buttons["URLBar.deleteButton"].tap()
+        waitForExistence(app.buttons["URLBar.deleteButton"])
+        app.buttons["URLBar.deleteButton"].firstMatch.tap()
         waitForExistence(app.staticTexts["Your browsing history has been erased."])
     }
 
