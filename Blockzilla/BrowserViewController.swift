314797af8a350e1e9b2b4c6605b3a24569107995
Bogdan Cernea <47420700+bogdancernea28@users.noreply.github.com>
2021-07-14 21:54:14 +0300

    Fixes #1948 - Browser - Top Toolbar Redesign (#2011)
    Co-authored-by: Bogdan Cernea <bogdan.cernea@softvision.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- Blockzilla/BrowserViewController.swift
+++ Blockzilla/BrowserViewController.swift
@@ -899,11 +899,6 @@ extension BrowserViewController: URLBarDelegate {
         toggleURLBarBackground(isBright: !webViewController.isLoading)
     }
 
-    func urlBarDidPressDelete(_ urlBar: URLBar) {
-        updateFindInPageVisibility(visible: false)
-        self.resetBrowser()
-    }
-
     func urlBarDidFocus(_ urlBar: URLBar) {
         let isOnHomeView = homeView != nil
         overlayView.present(isOnHomeView: isOnHomeView)
