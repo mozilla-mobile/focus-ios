0c0dbf94633d74a120d2b431f7d4d267c514e015
Stefan Arentz <stefan@arentz.ca>
2021-07-30 11:57:31 -0400

    Fixes #2056 - Include the String Tools into the Focus project

--- tools/Localizations/Package.swift
+++ tools/Localizations/Package.swift
@@ -5,16 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "Localizations",
+    platforms: [
+        .macOS(.v10_13)
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.4.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Localizations",
-            dependencies: []),
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]),
         .testTarget(
             name: "LocalizationsTests",
             dependencies: ["Localizations"]),
