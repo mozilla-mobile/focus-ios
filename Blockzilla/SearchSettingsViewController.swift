7a08bb4e7baa2527ca35c654d230d03b39f77eff
Bogdan Cernea <bogdan.cernea@softvision.com>
2021-08-18 10:09:12 +0300

    Fixes #1954 - Settings - Modern iOS Settings Layout

--- Blockzilla/SearchSettingsViewController.swift
+++ Blockzilla/SearchSettingsViewController.swift
@@ -9,15 +9,16 @@ protocol SearchSettingsViewControllerDelegate: class {
     func searchSettingsViewController(_ searchSettingsViewController: SearchSettingsViewController, didSelectEngine engine: SearchEngine)
 }
 
-class SearchSettingsViewController: UITableViewController {
+class SearchSettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     weak var delegate: SearchSettingsViewControllerDelegate?
 
     private let searchEngineManager: SearchEngineManager
     private var isInEditMode = false
+    private let tableView = UITableView(frame: .zero, style: .grouped)
 
     init(searchEngineManager: SearchEngineManager) {
         self.searchEngineManager = searchEngineManager
-        super.init(style: .insetGrouped)
+        super.init(nibName: nil, bundle: nil)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -30,6 +31,16 @@ class SearchSettingsViewController: UITableViewController {
         navigationItem.title = UIConstants.strings.settingsSearchLabel
         navigationController?.navigationBar.tintColor = .accent
         view.backgroundColor = .primaryBackground
+        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.bottom.equalTo(self.view)
+            make.leading.trailing.equalTo(self.view).inset(UIConstants.layout.settingsItemInset)
+        }
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .clear
         tableView.separatorColor = UIConstants.colors.settingsSeparator
         tableView.selectRow(at: IndexPath(row: 0, section: 1), animated: false, scrollPosition: .none)
         tableView.tableFooterView = UIView()
@@ -37,28 +48,21 @@ class SearchSettingsViewController: UITableViewController {
         navigationItem.rightBarButtonItem?.accessibilityIdentifier = "edit"
     }
 
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = " "
-        cell.backgroundColor = .primaryBackground
-
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {        
         if section == 0 {
-            let label = SmartLabel()
-            label.text = UIConstants.strings.InstalledSearchEngines
-            label.textColor = UIConstants.colors.tableSectionHeader
-            label.font = UIConstants.fonts.tableSectionHeader
-            cell.contentView.addSubview(label)
-
-            label.snp.makeConstraints { make in
-                make.leading.trailing.equalTo(cell.textLabel!)
-                make.centerY.equalTo(cell.textLabel!).offset(3)
-            }
+            let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+            cell.backgroundColor = .primaryBackground
+            cell.textLabel?.text = UIConstants.strings.InstalledSearchEngines
+            cell.textLabel?.font = UIConstants.fonts.tableSectionHeader
+            cell.textLabel?.textColor = UIConstants.colors.tableSectionHeader
+            
+            return cell
+        } else {
+            return nil
         }
-
-        return cell
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         if isInEditMode {
             return 1
         } else {
@@ -67,7 +71,7 @@ class SearchSettingsViewController: UITableViewController {
 
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let numberOfEngines = searchEngineManager.engines.count
         if isInEditMode { // NOTE: This is false when a user is swiping to delete but tableView.isEditing is true
             return numberOfEngines
@@ -80,7 +84,7 @@ class SearchSettingsViewController: UITableViewController {
         }
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let engines = searchEngineManager.engines
         if indexPath.item == engines.count {
             let cell = UITableViewCell(style: .default, reuseIdentifier: "addSearchEngine")
@@ -154,16 +158,16 @@ class SearchSettingsViewController: UITableViewController {
         }
     }
     
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath)
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath)
     {
         cell.roundedCorners(tableView: tableView, indexPath: indexPath)
     }
 
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return indexPath.row == searchEngineManager.engines.count+1 ? 44*2 : 44
     }
 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
 
         let engines = searchEngineManager.engines
@@ -188,7 +192,7 @@ class SearchSettingsViewController: UITableViewController {
         }
     }
 
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         let engines = searchEngineManager.engines
 
         if indexPath.row >= engines.count {
@@ -200,7 +204,7 @@ class SearchSettingsViewController: UITableViewController {
         return engine != searchEngineManager.activeEngine
     }
 
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             searchEngineManager.removeEngine(engine: searchEngineManager.engines[indexPath.row])
             tableView.reloadData()
