99f8dd258080989f8d1b42f42cdfa593952b880e
Stefan Arentz <stefan@arentz.ca>
2021-09-07 11:38:41 -0400

    Fixes #2092 - Capture telemetry for Shortcuts (#2232)

--- Blockzilla/AppDelegate.swift
+++ Blockzilla/AppDelegate.swift
@@ -414,6 +414,9 @@ extension AppDelegate {
         }
 
         Glean.shared.initialize(uploadEnabled: Settings.getToggle(.sendAnonymousUsageData))
+
+        // Send "at startup" telemetry
+        GleanMetrics.Shortcuts.shortcutsOnHomeNumber.set(Int64(ShortcutsManager.shared.numberOfShortcuts))
     }
 
     func presentModal(viewController: UIViewController, animated: Bool) {
