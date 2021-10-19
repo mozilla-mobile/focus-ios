f115b5946379e2242c543f746e1f85467a64488a
Razvan Litianu <litianu.razvan@gmail.com>
2021-10-19 09:47:10 +0300

    Remove empty file

--- Blockzilla/UITableViewCellExtensions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UITableViewCellExtensions.swift
-//  Blockzilla
-//
-//  Created by razvan.litianu on 18.10.2021.
-//  Copyright © 2021 Mozilla. All rights reserved.
-//
-
-import Foundation
