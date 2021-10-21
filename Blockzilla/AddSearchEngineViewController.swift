ae9dabdc6667294250ad00912c1e0f1d43836f7e
Bogdan Cernea <bogdan.cernea@softvision.com>
2021-10-21 11:11:23 +0300

    Fixes #2608 - Background colour of Settings background and table list should be reversed

--- Blockzilla/AddSearchEngineViewController.swift
+++ Blockzilla/AddSearchEngineViewController.swift
@@ -47,7 +47,7 @@ class AddSearchEngineViewController: UIViewController, UITextViewDelegate {
     }
 
     private func setupUI() {
-        view.backgroundColor = .secondarySystemBackground
+        view.backgroundColor = .systemGroupedBackground
         view.addSubview(scrollView)
 
         scrollView.snp.makeConstraints { (make) in
@@ -61,7 +61,7 @@ class AddSearchEngineViewController: UIViewController, UITextViewDelegate {
         container.addSubview(nameLabel)
 
         nameInput.attributedPlaceholder = NSAttributedString(string: UIConstants.strings.AddSearchEngineName, attributes: [.foregroundColor: UIColor.primaryText.withAlphaComponent(0.65)])
-        nameInput.backgroundColor = .systemBackground
+        nameInput.backgroundColor = .secondarySystemGroupedBackground
         nameInput.textColor = .primaryText
         nameInput.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: rowHeight))
         nameInput.leftViewMode = .always
@@ -73,10 +73,10 @@ class AddSearchEngineViewController: UIViewController, UITextViewDelegate {
         container.addSubview(nameInput)
 
         let templateContainer = UIView()
-        templateContainer.backgroundColor = .secondarySystemBackground
+        templateContainer.backgroundColor = .systemGroupedBackground
         container.addSubview(templateContainer)
 
-        templatePlaceholderLabel.backgroundColor = .systemBackground
+        templatePlaceholderLabel.backgroundColor = .secondarySystemGroupedBackground
         templatePlaceholderLabel.textColor = .primaryText.withAlphaComponent(0.65)
         templatePlaceholderLabel.text = UIConstants.strings.AddSearchEngineTemplatePlaceholder
         templatePlaceholderLabel.font = UIConstants.fonts.addSearchEngineInput
@@ -279,18 +279,18 @@ class AddSearchEngineViewController: UIViewController, UITextViewDelegate {
 
     func textViewDidBeginEditing(_ textView: UITextView) {
         templatePlaceholderLabel.isHidden = !textView.text.isEmpty
-        templateInput.backgroundColor = textView.text.isEmpty ? .clear : .systemBackground
+        templateInput.backgroundColor = textView.text.isEmpty ? .clear : .secondarySystemGroupedBackground
     }
 
     func textViewDidChange(_ textView: UITextView) {
         templatePlaceholderLabel.isHidden = !textView.text.isEmpty
-        templateInput.backgroundColor = textView.text.isEmpty ? .clear : .systemBackground
+        templateInput.backgroundColor = textView.text.isEmpty ? .clear : .secondarySystemGroupedBackground
         navigationItem.rightBarButtonItem?.isEnabled = !templateInput.text.isEmpty && !nameInput.text!.isEmpty
     }
 
     func textViewDidEndEditing(_ textView: UITextView) {
         templatePlaceholderLabel.isHidden = !textView.text.isEmpty
-        templateInput.backgroundColor = textView.text.isEmpty ? .clear : .systemBackground
+        templateInput.backgroundColor = textView.text.isEmpty ? .clear : .secondarySystemGroupedBackground
     }
 
     static func isValidTemplate(_ template: String) -> Bool {
