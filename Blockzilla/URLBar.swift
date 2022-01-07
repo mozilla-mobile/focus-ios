ef91954eba4e51f7d0caf38602d5a78cedd4cc42
Raluca Iordan <ralucaiordan@icloud.com>
2022-01-07 14:59:12 +0200

    Fixes #2935 - The hamburger menu and shield icon are not correctly displayed in landscape when the URL bar is hidden

--- Blockzilla/URLBar.swift
+++ Blockzilla/URLBar.swift
@@ -837,7 +837,7 @@ class URLBar: UIView {
         toolset.backButton.alpha = shouldShowToolset ? expandAlpha : 0
         toolset.forwardButton.alpha = shouldShowToolset ? expandAlpha : 0
         toolset.deleteButton.alpha = shouldShowToolset ? expandAlpha : 0
-        toolset.contextMenuButton.alpha = shouldShowToolset ? expandAlpha : 0
+        toolset.contextMenuButton.alpha = isEditing ? expandAlpha : (shouldShowToolset ? expandAlpha : 0)
 
         collapsedTrackingProtectionBadge.alpha = 0
         if isEditing {
