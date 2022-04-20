178676af0d2b32b41250c6c4e11774d8a42b48be
Catalin Andrei Neculaide <catalin.neculaide@softvision.com>
2022-04-20 10:20:20 +0300

    remove whitespace

--- Blockzilla/Pro Tips/ShareTrackersViewController.swift
+++ Blockzilla/Pro Tips/ShareTrackersViewController.swift
@@ -14,7 +14,7 @@ class ShareTrackersViewController: UIViewController {
         self.shareTap = shareTap
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
