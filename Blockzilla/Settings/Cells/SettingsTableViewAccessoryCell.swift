1a1d5e309f0df54b797ccb85b68c91346b2a198a
Raluca Iordan <ralucaiordan@icloud.com>
2022-07-25 11:12:06 +0300

    Fixes #3328 Add toggle to set Focus as default browser within Focus settings

--- Blockzilla/Settings/Cells/SettingsTableViewAccessoryCell.swift
+++ Blockzilla/Settings/Cells/SettingsTableViewAccessoryCell.swift
@@ -24,7 +24,7 @@ class SettingsTableViewAccessoryCell: SettingsTableViewCell {
         accessoryView = UIImageView(image: UIImage(systemName: "chevron.right"))
         tintColor = .secondaryText.withAlphaComponent(0.3)
     }
-    
+
     convenience init(style: UITableViewCell.CellStyle, reuseIdentifier: String?, hasAccessoryView: Bool = true) {
         self.init(style: style, reuseIdentifier: reuseIdentifier)
         accessoryView = hasAccessoryView ? UIImageView(image: UIImage(systemName: "chevron.right")) : nil
