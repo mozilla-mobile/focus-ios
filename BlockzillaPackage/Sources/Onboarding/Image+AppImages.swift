b347ad445cb802112811e5440a5a3bf1743abab0
Raluca Iordan <ralucaiordan@icloud.com>
2022-08-31 15:48:47 +0300

    Fixes  #3388 - Update onboarding screen 1

---
+++ BlockzillaPackage/Sources/Onboarding/Image+AppImages.swift
@@ -0,0 +1,11 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import SwiftUI
+
+public extension Image {
+    static let logo = Image("icon_logo", bundle: Bundle.myModule)
+    static let close = Image("icon_close", bundle: Bundle.myModule)
+    static let background = Image("icon_background", bundle: Bundle.myModule)
+}
