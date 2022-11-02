0824e0d65ac26768f9644338a520f84a487ab817
Raluca Iordan <ralucaiordan@icloud.com>
2022-11-02 16:07:29 +0200

    Fixes #3534 - Can't close shield menu when accessibility text is turned on

--- Blockzilla/Tracking Protection/TrackingProtectionViewController.swift
+++ Blockzilla/Tracking Protection/TrackingProtectionViewController.swift
@@ -217,7 +217,7 @@ class TrackingProtectionViewController: UIViewController {
         if case .browsing = state { return 2 }  else { return 1 }
     }
     private var tableViewTopInset: CGFloat {
-        if case .settings = state { return 0 }  else { return 48 }
+        if case .settings = state { return 0 }  else { return 54 }
     }
     var state: TrackingProtectionState
     let favIconPublisher: AnyPublisher<URL?, Never>?
@@ -303,7 +303,9 @@ class TrackingProtectionViewController: UIViewController {
            let baseDomain = browsingStatus.url.baseDomain {
             view.addSubview(header)
             header.snp.makeConstraints { make in
+                self.headerHeight = make.height.equalTo(72).constraint
                 make.top.leading.trailing.equalToSuperview()
+                make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
             }
             if let publisher = favIconPublisher {
                 header.configure(domain: baseDomain, publisher: publisher)
