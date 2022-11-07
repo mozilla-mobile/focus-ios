c67915364d7fef6ce572f3abc83922db78e64316
Jackie Johnson <107960801+jjSDET@users.noreply.github.com>
2022-11-07 13:18:25 -0600

    replace firefox with focus in swift task files

--- tools/export-strings.sh
+++ tools/export-strings.sh
@@ -15,15 +15,21 @@ fi
 echo "[*] Cloning mozilla-l10n/focusios-l10n"
 git clone https://github.com/mozilla-l10n/focusios-l10n.git
 
+echo "[*] Cloning mozilla-mobile/LocalizationTools"
+[ -d tools/Localizations ] && rm -rf tools/Localizations
+git clone https://github.com/mozilla-mobile/LocalizationTools.git tools/Localizations
+
 echo "\n\n[*] Building tools/Localizations"
 (cd tools/Localizations && swift build)
 
+echo "[*] Replacing firefox with focus in swift task files"
+sed -i '' 's/firefox-ios.xliff/focus-ios.xliff/g' tools/Localizations/Sources/LocalizationTools/tasks/*.swift
+
 echo "\n\n[*] Exporting Strings (output in export-strings.log)"
-(cd tools/Localizations && swift run Localizations \
+(cd tools/Localizations && swift run LocalizationTools \
   --export \
   --project-path "$PWD/../../Blockzilla.xcodeproj" \
   --l10n-project-path "$PWD/../../focusios-l10n") > export-strings.log 2>&1
 
 echo "\n\n[!] Hooray strings have been succesfully exported."
 echo "[!] You can create a PR in the focusios-l10n checkout"
-
