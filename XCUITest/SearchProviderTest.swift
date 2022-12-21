f7e857d2917bce06be1ae4e82a464b187705cf3a
Catalin Andrei Neculaide <catalin.neculaide@softvision.com>
2022-12-21 10:03:00 +0200

    updated the old tests for provider

--- XCUITest/SearchProviderTest.swift
+++ XCUITest/SearchProviderTest.swift
@@ -139,7 +139,6 @@ class SearchProviderTest: BaseTestCase {
         app.buttons["OverlayView.searchButton"].firstMatch.tap()
         waitForWebPageLoad()
 
-        urlbarUrltextTextField.tap()
 		// Check the correct site is reached
 		switch provider {
 			case "Google":
@@ -162,8 +161,5 @@ class SearchProviderTest: BaseTestCase {
 			default:
 				XCTFail("Invalid Search Provider")
 		}
-        if !iPad() {
-            cancelButton.tap()
-        }
 	}
 }
